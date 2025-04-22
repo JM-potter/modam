33fc7dd26068fb72bd1368db7e1917255d1499b1
JM-potter <jm@example.com>
2025-04-22 18:23:10 +0900

    page.tsx 연결 및 dynamic import 적용

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import dynamic from "next/dynamic";
 
-// 빌드 오류를 막기 위해 dynamic import 사용
+// 서버 사이드 렌더링 중 Calendar 에러를 피하기 위해 dynamic import 사용
 const ModamMainPage = dynamic(() => import("./ModamMainPage"), { ssr: false });
 
 export default function Page() {
